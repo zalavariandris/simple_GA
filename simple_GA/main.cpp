4b6fb6b947308508b6220932cf88affae2aec2b1
zalavariandris <zalavariandris@gmail.com>
2017-02-26 00:03:08 +0100

    Initial Commit

---
+++ simple_GA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simple_GA
+//
+//  Created by András Zalavári on 2017. 02. 26..
+//  Copyright © 2017. András Zalavári. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
